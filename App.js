f3712584f0c48ae04f030ac7104641e72fde1bb1
Ocampoing <62347041+Ocampoing@users.noreply.github.com>
2023-07-13 23:40:42 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import MainScreen from './src/Screens/MainScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Coder!</Text>
+      <MainScreen />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });
